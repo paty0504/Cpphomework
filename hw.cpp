bb4547a30bd8770b2b714520ef6cbcddb0a18261
Nguyễn Tiến Thành <37099218+paty0504@users.noreply.github.com>
2019-03-14 16:07:45 +0700

    Update hw.cpp

--- hw.cpp
+++ hw.cpp
@@ -4,12 +4,14 @@ int main(){
     char s[50] = "Tai khoan +6,438,000VND";
     
     int money = 0;
+    //Lay gia tri so tien
     for(int i = 0; s[i]; i++)
     {
         if(s[i] < '0'|| s[i] > '9' || s[i] == ',') continue;
         money = money*10 + (s[i] - '0');
     }
     cout<<money<<endl;
+    //Chuyen ve dang xau khong co dau cach
     int i = 0, j =0;
     while(s[i]){
         if(s[i] != ' ') s[j++] = s[i];
